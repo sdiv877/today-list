d2d295245efaa3f55d07d029f8e11564d37e93a2
sdiv877 <57572181+sdiv877@users.noreply.github.com>
2021-07-08 11:25:32 +1200

    Rename App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AppDrawer from './components/AppDrawer';
+import { HashRouter, Route } from 'react-router-dom';
+
+import CurrentTasks from './pages/CurrentTasks'
+import Settings from './pages/Settings'
+import CompletedTasks from './pages/CompletedTasks';
+import RecycleBin from './pages/RecycleBin';
+import Stats from './pages/Stats';
+
+import './styles/App.css'
+
+// Treat this as the <App /> portion of the project
+// Anything inside ReactDOM.render() is 'returned'
+ReactDOM.render(
+    <div className="App">
+        <HashRouter>
+            <AppDrawer />
+
+            <div className="content">
+                <main>
+                    <Route exact path="/" component={CurrentTasks} />
+                    <Route exact path="/completed" component={CompletedTasks} />
+                    <Route exact path="/bin" component={RecycleBin} />
+                    <Route exact path="/stats" component={Stats} />
+                    <Route path="/settings" component={Settings} />
+                </main>
+            </div>
+        </HashRouter>
+    </div>
+    , document.body);
